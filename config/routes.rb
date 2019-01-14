08c22260fc5fed2b475c2f2e7bdc5a922907961f
thekernel <salut.amos@gmail.com>
2019-01-14 00:07:49 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,32 @@ Rails.application.routes.draw do
   get "/dashboard" => "dashboard#index" , as: :dashboard
   get "/users"     => "custom_users#index", as: :users 
 
+  post "/users/new"     => "custom_users#create", as: :create_user
+	get "/users/new"     => "custom_users#new", as: :new_user
+	get "/user/edit/:id" => "custom_users#edit", as: :edit_user
+
+	patch "/user/update/:id" => "custom_users#update", as: :udapte_user
+
+	#get "/commissions/settings/new"     => "commission_settings#new", as: :new_commission_setting
+	#get "/commissions/settings/edit/:id" => "commission_settings#edit", as: :edit_commission_setting
+
+	delete "/user/destroy/:id" => "custom_users#destroy", as: :destroy_user
+	
+	delete "/credits/destroy/:id" => "credits#destroy", as: :destroy_credit
+	get "/user/delete/:id" => "custom_users#delete", as: :delete_user
+
+
+	get "/user/enable/:id" => "custom_users#get_enable", as: :get_enable_user_account
+	post "/user/enable/:id" => "custom_users#post_enable", as: :post_enable_user_account
+
+	get "/user/disable/:id" => "custom_users#get_disable", as: :get_disable_user_account
+	post "/user/disable/:id" => "custom_users#post_disable", as: :post_disable_user_account
+
+	get "/user/update/:id" => "custom_users#update", as: :update_user
+
+
+	get "/user/show/:id" => "custom_users#show", as: :show_user
+
 	#get "/customers" => "customers#index", as: :customers_index
   #devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
