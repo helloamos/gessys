ac18a5ad88e8adc384a7c485f77f62451124028b
thekernel <salut.amos@gmail.com>
2019-03-11 21:47:16 +0000

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
-  after_action :flash_to_headers
+  #after_action :flash_to_headers
   # Include Application helper.
 	include ApplicationHelper
  
